2e4a819dada2ca06c1761e8adc833594b5719ce3
Chris Hughes <naokotani@protonmail.com>
2022-06-17 03:57:11 -0300

    Changed how posts component works
    posts are no longer in a grid, but are stacked with the top post
    expanded.
    
    Added buttons to cycle through posts.

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import BlockContent from '@sanity/block-content-to-react';
 import serializers from '../components/serializers';
 import Figure from '../components/Figure'
@@ -9,7 +9,7 @@ const Events = ({ sort, number }) => {
 
 	const data = useStaticQuery(graphql`
 query {
-  allSanityPost(filter: {type: {eq: "Event"}}, limit: 10) {
+  allSanityPost(limit: 10) {
     edges {
       node {
         title
@@ -33,37 +33,67 @@ query {
 }
 `)
 
+	const [posts, setPosts] = useState([])
+	const [postNum, setPostNum] = useState(number)
+
 	// If here are enough event posts then slice the array of posts up
 	// to that number, or if it is less, then diplay all posts. 
-	let posts;
-	if (data.allSanityPost.edges.length > number) {
-		posts = data.allSanityPost.edges.slice(0, number);
-	} else {
-		posts = data.allSanityPost.edges
+
+	useEffect(() => {
+		if (data.allSanityPost.edges.length > postNum) {
+			setPosts(data.allSanityPost.edges.slice(0, postNum));
+		} else {
+			setPosts(data.allSanityPost.edges);
+		}
+	}, [postNum])
+
+	const handleNext = () => {
+		setPosts([
+			...posts.slice(postNum - 1),
+			...posts.slice(0, postNum - 1)
+		])
 	}
 
-	console.log(posts)
+	const handlePrev = () => {
+		setPosts([
+			...posts.slice(1),
+			...posts.slice(0, 1)
+		])
+	}
+
 
 	// Create an h3 for each event post title.
 	return (
-		<div className="">
-			<ul className="post--layout">
-			{posts.map((post) => (
-				<div className="post--card card grid">
-					<article className="aside-left">
-						<h3>{post.node.title}</h3>
-						<p>{post.node.date}</p>
-						<BlockContent
-							blocks={post.node._rawBody}
-							serializers={serializers} />
-					</article>
-					<aside className="aside-right">
-						<Figure id={post.node.mainImage.asset._id} />
-					</aside>
-				</div>
-			))}
+		<div className="post">
+			<div className="underline">
+				<h2>Upcoming news and Events</h2>
+			</div>
+			<button
+				className="post--next"
+				onClick={() => handleNext()}>&#8592;</button>
+			<button
+				onClick={() => handlePrev()}>&#8594;</button>
+			<ul className="">
+				{posts[0] &&
+					<div className="post--card card">
+						<article className="aside-left">
+							<h3>{posts[0].node.title}</h3>
+							<p>{posts[0].node.date}</p>
+							<BlockContent
+								blocks={posts[0].node._rawBody}
+								serializers={serializers} />
+						</article>
+					</div>
+				}
+				{posts.slice(1).map((post) => (
+					<div className="post--card card">
+						<article className="aside-left">
+							<h5>{post.node.title}</h5>
+						</article>
+					</div>
+				))}
 			</ul>
-		</div>
+		</div >
 	)
 
 }
