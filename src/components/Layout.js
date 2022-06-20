aefc9c55947c6ae6588467a3a11f0881a944764d
Chris Hughes <naokotani@protonmail.com>
2022-06-20 10:06:08 -0300

    Added cart that tracks if items are cart
    If there are items in the cart, cart will have a plus sign
    
    Removed old test cold from layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { StaticImage } from "gatsby-plugin-image";
 import { useStaticQuery, graphql, Link } from "gatsby";
-import Client from 'shopify-buy';
+import { useCartCount } from "../context/StoreContext"
 import "./layout/layout.css";
 import "./layout/normalize.css";
 
@@ -34,42 +34,16 @@ export default function Layout({ children, checkout }) {
 		}
   `);
 
-	// async function checkout() {
-	// 	// build a client
-	// 	console.log('foo');
-	// 	const client = Client.buildClient({
-	// 		storefrontAccessToken: '8b8ef58e1ae3085248ff54cb7cdf8e07',
-	// 		domain: `ark-webstore.myshopify.com`,
-	// 	})
 
-	// 	// create a checkout
-	// 	const checkout = await client.checkout.create()
-
-	// 	// create an array of line items
-	// 	console.log(data.allShopifyProduct.edges[0].node.variants[0].shopifyId)
-	// 	const variantId = data.allShopifyProduct.edges[0].node.variants[0].shopifyId;
-	// 	console.log(variantId)
-
-
-	// 	const lineItemsToAdd = [{ variantId, quantity: 1 }];
-
-	// 	// add line items to the checkout
-	// 	const checkoutId = checkout.id
-	// 	const newCheckout = await client.checkout.addLineItems(
-	// 		checkoutId,
-	// 		lineItemsToAdd
-	// 	)
-
-	// 	// finish the checkout by visiting webUrl
-	// 	window.open(checkout.webUrl)
-	// }
+	const cartCount = useCartCount();
+	console.log(cartCount)
 
 	// // Remove the home page link with the slug "l-arche-cape-breton"
 	const links = data.allSanityPage.edges.filter(
 		(edge) => edge.node.slug.current !== "l-arche-cape-breton"
-	);
-	// // return the header with the nav, logo/link, link menu, and the footer. Children are
-	// // in the main tag.
+	)
+	// return the header with the nav, logo/link, link menu, and the footer. Children are
+	// in the main tag.
 
 	return (
 		<div>
@@ -77,10 +51,7 @@ export default function Layout({ children, checkout }) {
 				<Link to="/">
 					<StaticImage src="../images/lcblogohd.png" alt="lol" />
 				</Link>
-				<nav>
-					<button
-						className=""
-						onClick={() => checkout()}>BUY</button>
+				<nav className="main-nav">
 					{links.map((link) => (
 						<Link
 							key={link.node.slug.current}
@@ -89,7 +60,19 @@ export default function Layout({ children, checkout }) {
 							{link.node.link}
 						</Link>
 					))}
-					<Link to="/products">Store</Link>
+					<Link className="store" to="/products">Store</Link>
+					{cartCount === 0 &&
+						<StaticImage
+							src="../images/cart-shopping-solid.svg"
+							widtth="22.5"
+							height="22.5" />
+					}
+					{cartCount > 0 &&
+						<StaticImage
+							src="../images/cart-plus-solid.svg"
+							widtth="22.5"
+							height="22.5" />
+					}
 				</nav>
 			</header>
 			<main className="layout">{children}</main>
