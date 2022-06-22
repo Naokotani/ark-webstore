5f7ac0f6d01ffe29bd1965ef18c42e317458275e
Chris Hughes <naokotani@protonmail.com>
2022-06-22 15:30:51 -0300

    Put data in checkout.js into a table.
    added some CSS to layout.css to handle numbers in tables.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,7 +3,8 @@ import {
 	useCartItems,
 	useCheckout,
 	useCartTotals,
-  useRemoveItemFromCart,
+	useRemoveItemFromCart,
+	useAddItemToCart,
 }
 	from "../context/StoreContext";
 import Layout from "../components/Layout";
@@ -13,7 +14,10 @@ const Checkout = () => {
 	const checkout = useCheckout();
 	const cartItems = useCartItems();
 	const totals = useCartTotals();
-  const removeItem = useRemoveItemFromCart();
+	const removeItem = useRemoveItemFromCart();
+	const addItem = useAddItemToCart();
+
+	const [quantity, setQuantity] = useState(1);
 
 	console.log(totals)
 	console.log(cartItems)
@@ -24,59 +28,56 @@ const Checkout = () => {
 
 	const handleRemove = (id) => {
 		removeItem(id);
-		
+
 	}
+
+	const handleQuantity = (id) => {
+		const q = parseInt(quantity)
+		removeItem(id);
+		addItem(id, q)
+	}
+
+	//const handleQuantitState = () => {
+	//set
+	//}
 
 	return (
 		<Layout>
+			<table>
+				<tr>
+					<th>Header</th>
+					<td>content</td>
+					<td>content</td>
+					<td>content</td>
+				</tr>
+			</table>
 			{!cartItems &&
 				<h1>Your Cart is Empty</h1>
 			}
-			{cartItems &&
-				<table>
+			<table>
+				<thead>
 					<tr>
 						<th>Item</th>
 						<th>quantity</th>
 						<th>price</th>
-
 					</tr>
-					<td>
-						{cartItems.map(item => (
-							<tr key={item.id}>
-								{item.title}
-							</tr>
-						))
-						}
-					</td>
-					<td>
-						{cartItems.map(item => (
-							<tr key={item.id}>
-								{item.quantity}
-							</tr>
-						))
-						}
-					</td>
-					<td>
-						{cartItems.map(item => (
-							<tr key={item.id}>
-								{item.variant.priceV2.amount}
-							</tr>
-						))
-						}
-					</td>
-					<td>
-						{cartItems.map(item => (
-							<tr key={item.id}>
+				</thead>
+				{cartItems.map(item => (
+					<tbody>
+						<tr>
+							<td>{item.title}</td>
+							<td className="number">{item.quantity}</td>
+							<td className="number">{item.variant.priceV2.amount}</td>
+							<td scope="col" key={item.id}>
 								<button
 									onClick={() => handleRemove(item.id)}>
 									Remove</button>
-							</tr>
-						))
-						}
-					</td>
-				</table>
-			}
-			<p>{totals.total}</p>
+							</td>
+						</tr>
+					</tbody>
+				))}
+			</table>
+			<p>Subtotal: {totals.total}</p>
 			<button onClick={() => handleCheckout()}>Checkout</button>
 		</Layout>
 	)
