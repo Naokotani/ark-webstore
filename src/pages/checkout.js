ee47e956c1b521d91a7183d1950ab85778929616
Chris Hughes <naokotani@protonmail.com>
2022-06-23 01:56:32 -0300

    Added update functionality to checkout.
    Added a new function to StoreContext.js to handle updates.

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,6 +5,7 @@ import {
 	useCartTotals,
 	useRemoveItemFromCart,
 	useAddItemToCart,
+	useUpdateItem,
 }
 	from "../context/StoreContext";
 import Layout from "../components/Layout";
@@ -16,26 +17,36 @@ const Checkout = () => {
 	const totals = useCartTotals();
 	const removeItem = useRemoveItemFromCart();
 	const addItem = useAddItemToCart();
+	const updateItem = useUpdateItem();
 
-	const [quantity, setQuantity] = useState(1);
 
-	console.log(totals)
-	console.log(cartItems)
+	const [quantity, setQuantity] = useState({});
+
 
 	const handleCheckout = () => {
 		checkout();
 	}
 
-	const handleRemove = (id) => {
-		removeItem(id);
-
+	const handleUpdate = (checkoutId, value) => {
+		setQuantity({ ...quantity, [checkoutId]: value })
+		const lineItemsToUpdate = [
+			{ id: checkoutId, quantity: parseInt(value) }
+		];
+		updateItem(checkoutId, lineItemsToUpdate)
 	}
 
-	const handleQuantity = (id) => {
-		const q = parseInt(quantity)
-		removeItem(id);
-		addItem(id, q)
+	const handleRemove = (itemId) => {
+		removeItem(itemId)
 	}
+
+	useEffect(() => {
+		let updateQuantity = {}
+		cartItems.forEach(item => {
+			updateQuantity[item.id] = item.quantity
+		})
+		setQuantity(updateQuantity)
+		console.log('useEffect')
+	}, [cartItems])
 
 	return (
 		<Layout>
@@ -44,29 +55,46 @@ const Checkout = () => {
 				<h1>Your Cart is Empty</h1>
 			}
 			{cartItems &&
-			<table>
-				<thead>
-					<tr>
-						<th className="border">Item</th>
-						<th className="border">quantity</th>
-						<th className="border">price</th>
-					</tr>
-				</thead>
-				{cartItems.map(item => (
-					<tbody>
+				<table>
+					<thead>
 						<tr>
-							<td className="border">{item.title}</td>
-							<td className="number border">{item.quantity}</td>
-							<td className="number border">{item.variant.priceV2.amount}</td>
-							<td key={item.id}>
-								<button
-									onClick={() => handleRemove(item.id)}>
-									Remove</button>
-							</td>
+							<th className="border">Item</th>
+							<th className="border">quantity</th>
+							<th className="border">price</th>
 						</tr>
-					</tbody>
-				))}
-			</table>
+					</thead>
+					{cartItems.map(item => {
+						console.log(quantity)
+						return (
+							<tbody>
+								<tr>
+									<td className="border">{item.title}</td>
+									<td className="number border">{item.quantity}</td>
+									<td className="number border">{item.variant.priceV2.amount}</td>
+									<td key={item.id}>
+										<button
+											onClick={() => handleRemove(item.id)}>
+											Remove</button>
+									</td>
+									{ quantity &&
+									<td>
+										<select
+											value={quantity[item.id]}
+											name="quantity"
+											onChange={(e) => handleUpdate(item.id, e.target.value)}>
+											<option value={1}>one</option>
+											<option value={2}>two</option>
+											<option value={3}>three</option>
+											<option value={4}>four</option>
+											<option value={5}>five</option>
+										</select>
+									</td>
+									}
+								</tr>
+							</tbody>
+						)
+					})}
+				</table>
 			}
 			<p>Subtotal: {totals.total}</p>
 			<button onClick={() => handleCheckout()}>Checkout</button>
