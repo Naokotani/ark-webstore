43c85e96101a27dce3beb4194e3b47d822f174d8
Chris Hughes <naokotani@protonmail.com>
2022-06-24 06:53:13 -0300

    Added data to house template

--- src/templates/house.js
+++ src/templates/house.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Layout from "../components/Layout"
+import Figure from '../components/Figure'
+import serializers from "../components/serializers";
+import BlockContent from "@sanity/block-content-to-react";
 
-const House = ({pageContext}) => {
-	console.log(pageContext)
-  return (
+const House = ({ pageContext }) => {
+	console.log(pageContext.body)
+	return (
 		<Layout>
 			<article className="post">
-			<header className="flex underline">
-			<h2>{pageContext.name}</h2>
-				<h3>{pageContext.address}</h3>
-			</header>
+				<header className="flex underline">
+					<h2>{pageContext.name}</h2>
+					<h3>{pageContext.address}</h3>
+				</header>
+				<Figure id={pageContext.image.asset._id} alt="" />
+				<BlockContent
+					blocks={pageContext.body}
+					serializers={serializers} />
 			</article>
 		</Layout>
 	);
