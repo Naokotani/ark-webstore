0d9eec4dd63b134d98d9ea33f05d46c40bbd28a6
Chris Hughes <naokotani@protonmail.com>
2022-06-26 07:51:20 -0300

    Refactored Role to be more readable

--- src/components/Role.js
+++ src/components/Role.js
@@ -41,34 +41,48 @@ const Role = (props) => {
     }
   `);
 
-	//Find the matching role from the role reference
-	const role = data.allSanityRoleComponent.edges.filter(
-		(role) => role.node._id === props.node._ref
-	);
+	function findRoleComp() {
+		const role = data.allSanityRoleComponent.edges.filter(
+			(role) => role.node._id === props.node._ref
+		);
+		return role[0].number;
+	}
 
-	//Copy the list of person objects
-	let p = data.allSanityPerson.edges;
-
-	//Create an empty list for people
-	let people = [];
-
-	/* If the number is true and the lenth of the list of people is equal to 
-or greater than nubmer then while the people list is shorter than number
-add a random person that has not been added to the people list. */
-	if (role[0].node.number && p.length >= role[0].node.number) {
+	function randomPersonArry(role, p) {
+		let people = [];
 		while (people.length < role[0].node.number) {
 			const item = p[Math.floor(Math.random() * p.length)];
 			if (!people.includes(item)) {
 				people.push(item);
 			}
 		}
-	} else if (people.length < role[0].node.number) {
-		console.error("Not enough Items in array.");
-		//Otherwise finde the specific person to display
-	} else {
-		const names = role[0].node.specific.map(e => { return e.name })
-		people = p.filter(person => names.some(name => name === person.node.name))
+		return people
 	}
+
+	function findSpecificPerosn(role, p) {
+		let people = [];
+		const names = role.specific.map(e => { return e.name })
+		people = p.filter(person => names.some(name => name === person.node.name))
+		return people;
+	}
+
+	function createPeopleArr(role) {
+		let p = data.allSanityPerson.edges;
+		let people;
+
+		if (role.number && p.length >= role.number) {
+			people = randomPersonArry(role, p)
+		} else if (people.length < role.number) {
+			console.error("Not enough Items in array.");
+			//Otherwise finde the specific person to display
+		} else {
+			people = findSpecificPerosn(role, p)
+		}
+		return people;
+	}
+
+	const role = findRoleComp()
+	const people = createPeopleArr(role)
 
 	return (
 		<div className="post">
